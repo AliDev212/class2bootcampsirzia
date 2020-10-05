d3b148f9b03f573e304897e3f81e6eca24cda522
AliDev212 <ali212salman1999@gmail.com>
2020-10-05 11:24:11 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,15 +10,17 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
           <br/>
-          Updated by Ali Salman Hassan.
+          Base React App.
+          <br/>
+          Created by Ali Salman Hassan.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          className="Github-link"
+          href="https://github.com/AliDev212/class2bootcampsirzia"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Github Link
         </a>
       </header>
     </div>
